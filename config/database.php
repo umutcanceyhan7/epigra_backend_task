00ec9800ac1305372177aa59b0a41ba65b0ac0fc
Umutcan <umutcanceyhan@gmail.com>
2021-07-07 20:05:17 +0300

    Database configuration is added.

--- config/database.php
+++ config/database.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Str;
 
 return [
+    # HEROKU DATABASE URL 
+    $DATABASE_URL = parse_url('postgres://syfkbiramguzem:eae1f7df99b56b00501bcdd1098c2917868116d0ffda3fcc7a305306695964a0@ec2-54-227-246-76.compute-1.amazonaws.com:5432/db331atjb30617'),
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +67,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
